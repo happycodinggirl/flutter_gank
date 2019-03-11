96ce1850f492fee5a0e77bc8e389235b7babbccb
lily <hxlily86@gmail.com>
2019-03-11 16:11:14 +0800

    将组建拆分开来写，不要都写在同一个文件里面

--- lib/ChildCatergoryPage.dart
+++ lib/ChildCatergoryPage.dart
@@ -2,10 +2,10 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_app1/tasks/CatergoryChildTask.dart';
 import 'package:flutter_app1/presenters/CatergoryPresenter.dart';
 import 'package:flutter_app1/views/CatergoryChildView.dart';
+import 'package:flutter_app1/widgets/ListViewM.dart';
 import 'package:flutter_app1/model/CatergoryChild.dart';
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+
 
 class ChildCatergoryPage extends StatefulWidget{
 
@@ -61,70 +61,7 @@ class ChildState extends State<ChildCatergoryPage>  implements CatergoryChildVie
         body:Center(child: CircularProgressIndicator(),),
       );
     }
-      
-   /* Iterable<Widget> listTiles=catergoryChildList.map((item){
-      return buildSingleTile(context, item);
-    }).toList();
-
-    listTiles=ListTile.divideTiles(tiles: listTiles,context: context);*/
-
-  /*  return  new Scaffold(
-      body:ListView.separated(
-          itemBuilder: (BuildContext context,int index){
-        CatergoryChild catergoryChild=catergoryChildList[index];
-        return new ListTile(leading:getImage(catergoryChild.icon),
-          title:Text(catergoryChild.title),
-          subtitle: new Text(catergoryChild.created_at),//子item的内容
-          trailing: new Icon(Icons.arrow_right,color: Colors.green,),//显示右侧的箭头，不显示则传null
-        );
-      },
-        separatorBuilder:(context,index){
-            return new Divider(height: 1,);
-        },
-      itemCount: catergoryChildList.length,
-
-      ),
-      );*/
-
-    return  new Container(
-      child:ListView.separated(
-        itemBuilder: (BuildContext context,int index){
-          CatergoryChild catergoryChild=catergoryChildList[index];
-          return getItem(catergoryChild,index);
-        },
-        separatorBuilder:(context,index){
-          return new Divider(height: 1,);
-        },
-        itemCount: catergoryChildList.length,
-
-
-
-      ),
-    );
-
-
-
-
-  }
-
-  Widget getItem(CatergoryChild item,int index){
-    return GestureDetector(
-      onTap: (){
-        final snackBar = new SnackBar(content: new Text('第$index项被点击了!'));
-
-        Scaffold.of(context).showSnackBar(snackBar);
-      },
-        child:Padding(
-      padding: EdgeInsets.only(left:10,top:0,right:10,bottom:0),
-      child: Row(children: <Widget>[
-        Column(children: <Widget>[new FadeInImage.memoryNetwork(image:item.icon,placeholder:kTransparentImage
-          ,fit: BoxFit.cover,width: 100,height: 100,)],crossAxisAlignment: CrossAxisAlignment.start,),
-        Column(children: <Widget>[Container(margin: new EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
-          child:Text(item.title,style: TextStyle(color: Colors.black,fontSize: 20),) ,alignment:FractionalOffset.topLeft,)
-        ,Container(margin: new EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),child:Text(item.created_at,style: TextStyle(color: Colors.grey,fontSize: 15),) )],)
-      ],),
-
-    ));
+    return  new ListViewM(catergoryChildList);
   }
 
   @override
---
+++ lib/widgets/ListViewM.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_app1/model/CatergoryChild.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:flutter/material.dart';
+
+typedef onItemClickListener = void Function(int position);
+
+class ItemView extends StatelessWidget {
+  CatergoryChild item;
+  int index;
+  onItemClickListener listener;
+
+  ItemView(this.item, this.index, this.listener);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(//为了添加点击水波纹效果而加上的
+      padding: const EdgeInsets.all(10.0),
+      child: InkWell(
+          onTap: () {
+            listener(index);
+          },
+          child: Padding(
+            padding: EdgeInsets.only(left: 10, top: 0, right: 10, bottom: 0),
+            child: Row(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    new FadeInImage.memoryNetwork(
+                      image: item.icon,
+                      placeholder: kTransparentImage,
+                      fit: BoxFit.cover,
+                      width: 100,
+                      height: 100,
+                    )
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      margin: new EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
+                      child: Text(
+                        item.title,
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
+                      alignment: FractionalOffset.topLeft,
+                    ),
+                    Container(
+                        margin: new EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
+                        child: Text(
+                          item.created_at,
+                          style: TextStyle(color: Colors.grey, fontSize: 15),
+                        ))
+                  ],
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+class ListViewM extends StatelessWidget {
+  List<CatergoryChild> catergoryChildList;
+
+  ListViewM(this.catergoryChildList);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemBuilder: (BuildContext context, int index) {
+        CatergoryChild catergoryChild = catergoryChildList[index];
+        return ItemView(catergoryChild, index, (index) {
+          Scaffold.of(context)
+              .showSnackBar(SnackBar(content: Text("第$index项被点击了")));
+        });
+      },
+      separatorBuilder: (context, index) {
+        return new Divider(
+          height: 1,
+        );
+      },
+      itemCount: catergoryChildList.length,
+    );
+  }
+}
